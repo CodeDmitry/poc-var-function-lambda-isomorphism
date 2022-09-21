6c426238a912e347da91756e276adac702f6f8a7
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-09-21 06:32:33 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 // | The four examples below have the same observeable behavior.
 // | Comparing them on jsben.ch may surprise you.
 
-// | Example 1. 
+// | Example 1(IIFE). 
 (function() {
     var a = 2;
     var b = 3;
@@ -9,7 +9,7 @@
     console.log(a, b, c);
 })();
 
-// | Example 2. 
+// | Example 2(IIFE). 
 (function(a) {
     (function (b) {
         (function (c) {
@@ -18,7 +18,7 @@
     })(3);
 })(2);
 
-// | Example 3. 
+// | Example 3(IIFE). 
 (a => b => c => {
     console.log(a, b, c)
 })(2)(3)(5);
